2a2b197e4d0a45bdad7c2c0bc9317367d7286515
Matt Dobler <mdobler@gmail.com>
2017-02-23 11:32:12 -0800

    simple script to glue together animations

--- movie.sh
+++ movie.sh
@@ -2,7 +2,7 @@
 rm output/*.png
 for t in $(seq 1 1000 50000)
 do
-   ./renderer -d 255 255 -i $t -p 120 -a .0052 -g .95 --dt .16 --ppp 1.1 -s 30643149
+   ./renderer -d 255 255 -i $t -p 120 -a .0082 -g .95 --dt .16 --ppp 1.1 -s 43149
 done
 convert -delay 5 -loop 0 output/*.png animation.gif
 
